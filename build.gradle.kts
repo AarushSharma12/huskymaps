2b2145af68eb38564d0ba03a351cee4a7fbe3c41
aarushsharma <as2027@uw.edu>
2024-12-14 14:25:19 -0500

    dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,16 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    id("com.gradleup.shadow") version "8.3.5" // Ensure Shadow plugin is declared here
     java
 }
 
+tasks.jar {
+    manifest.attributes["Main-Class"] = "MapServer" // Add Main-Class for the entry point
+}
+
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17 // Ensure Java version compatibility
 }
 
 repositories {
@@ -16,7 +21,7 @@ repositories {
 dependencies {
     implementation("commons-codec:commons-codec:1.17.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
-    implementation("io.javalin:javalin:6.2.0")
+    implementation("io.javalin:javalin:6.3.0") // Update Javalin version to 6.3.0
     implementation("org.locationtech.spatial4j:spatial4j:0.8")
     implementation("org.slf4j:slf4j-simple:2.0.13")
 
